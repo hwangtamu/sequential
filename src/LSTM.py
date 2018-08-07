4449dedc00c88c437ded4d65187a5115a0e72332
hwangtamu <whxjtu@gmail.com>
2018-08-07 00:35:42 -0500

    restructure (incomplete)

--- src/LSTM.py
+++ src/LSTM.py
@@ -18,7 +18,149 @@ from keras.models import load_model
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
+from tensorflow.examples.tutorials.mnist import input_data
 from utils import Splice
+
+
+class LSTMClassifier(object):
+    def __init__(self,
+                 time_steps,
+                 n_units,
+                 n_inputs,
+                 n_classes,
+                 batch_size = 128,
+                 n_epochs = 150):
+        self.time_steps = time_steps
+        self.n_units = n_units
+        self.n_inputs = n_inputs
+        self.n_classes = n_classes
+        self.batch_size = batch_size
+        self.n_epochs = n_epochs
+        self.model = None
+
+        # Internal
+        self._data_loaded = False
+        self._trained = False
+        self.lstm_model, self.dense_model = None, None
+
+    def load_data(self, task):
+        if task=='mnist':
+            self.mnist = input_data.read_data_sets("mnist", one_hot=True)
+        if task=='dna':
+            self.splice = Splice('splice.csv')
+
+        self._data_loaded=True
+
+    def create_model(self,
+                     loss='categorical_crossentropy',
+                     metric='accuracy'):
+        self.model = Sequential()
+        self.model.add(LSTM(self.n_units, input_shape=(self.time_steps, self.n_inputs)))
+        self.model.add(Dense(self.n_classes, activation='softmax'))
+
+        self.model.compile(loss=loss,
+                           optimizer='rmsprop',
+                           metrics=[metric])
+
+    def train_model(self,
+              x,
+              y,
+              p,
+              save_model=False):
+        self.create_model()
+        self.model.fit(x, y, batch_size=self.batch_size, epochs=self.n_epochs, shuffle=False)
+        self._trained = True
+
+        if save_model:
+            self.model.save(p)
+
+    def build_models(self, model=None):
+        if not model:
+            model = self.model
+
+        lstm_model = Sequential()
+        lstm_model.add(LSTM(self.n_units, input_shape=(None, self.n_inputs), return_sequences=True))
+        weights = model.layers[0].get_weights()
+        lstm_model.layers[0].set_weights(weights)
+
+        dense_model = Sequential()
+        dense_model.add(Dense(self.n_classes, input_dim=self.n_units, activation='softmax'))
+        weights = model.layers[-1].get_weights()
+        dense_model.layers[0].set_weights(weights)
+        self.lstm_model, self.dense_model = lstm_model, dense_model
+
+    def eval_model(self,
+                   x,
+                   y,
+                   model=None):
+        model = load_model(model) if model else self.model
+        test_loss = model.evaluate(x, y)
+        print(test_loss)
+
+    def get_hidden_states(self,
+                          x,
+                          samples,
+                          model=None):
+        step = self.time_steps
+
+        model = load_model(model) if model else self.model
+        self.build_models(model)
+
+        hidden_states = []
+
+        for i in range(samples):
+            hidden_states += [hidden(self.lstm_model, np.array(x[i].reshape((-1, step, self.n_inputs))))]
+        return hidden_states
+
+
+def hidden(model, data):
+    # model = load_model(model) if model else self.model
+    intermediate_layer_model = Model(inputs=model.input, outputs=model.layers[0].output)
+    intermediate_output = intermediate_layer_model.predict(data)
+    return intermediate_output
+
+class MnistLSTMClassifier(LSTMClassifier):
+    def __init__(self):
+        LSTMClassifier.__init__(self,
+                                time_steps=28,
+                                n_units=128,
+                                n_inputs=28,
+                                n_classes=10)
+
+    def __load_data(self):
+        self.load_data('mnist')
+
+    def train(self, save_model=False):
+        if not self._data_loaded:
+            self.__load_data()
+
+        x_train = [x.reshape((-1, self.time_steps, self.n_inputs)) for x in self.mnist.train.images]
+        x_train = np.array(x_train).reshape((-1, self.time_steps, self.n_inputs))
+
+        self.train_model(x_train, self.mnist.train.labels, p="./saved_model/lstm-model_256.h5", save_model=save_model)
+
+    def evaluate(self, model=None):
+        if self._trained == False and model == None:
+            errmsg = "[!] Error: classifier wasn't trained or classifier path is not precised."
+            print(errmsg, file=sys.stderr)
+            sys.exit(0)
+
+        if not self._data_loaded:
+            self.__load_data()
+
+        x_test = [x.reshape((-1, self.time_steps, self.n_inputs)) for x in self.mnist.test.images]
+        x_test = np.array(x_test).reshape((-1, self.time_steps, self.n_inputs))
+
+        self.eval_model(x_test, self.mnist.test.labels)
+
+    def real_time_predict(self, vec, num=None):
+        output = self.dense_model.predict(vec)
+        if num == None:
+            output = [max(x) for x in output]
+        else:
+            output = [x[num] for x in output]
+        result = self.dense_model.predict_classes(vec)
+        return result[28], list(result), output
 
 
 class MnistLSTMClassifier(object):
